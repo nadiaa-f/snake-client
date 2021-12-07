ca494c6ac8250d9953bcc8d4002fb0dac29e094d
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-07 22:58:28 +0000

    refactoring + create an input module which will focus on managing the user input.

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
-const net = require("net");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const connect = require("./client");
+console.log("Connecting ...");
+connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = input => {
-
-  if (input === '\u0003') {
-    process.exit();
-  }
-
-};
-module.exports = setupInput;
+setupInput();
