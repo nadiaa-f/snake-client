8815cf036c9c29784177a50e2ff1e01541db29f4
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-07 22:01:28 +0000

    move the connection logic into its own module. We can call this the client module.

--- play.js
+++ play.js
@@ -1,25 +1,3 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("connect", (data) => {
-    console.log("connected to server");
-  })
-
-  conn.on("data", (data) => {
-    console.log("message from server: ", data);
-  })
-  
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+const connect = require("./client");
